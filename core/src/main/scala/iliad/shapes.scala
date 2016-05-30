0262741a18470594794dabd0c3ae2c3b75341e35
Yilin Wei <yilin.wei10@gmail.com>
2016-05-31 00:19:58 +0100

    add win32 windowing

--- core/src/main/scala/iliad/shapes.scala
+++ core/src/main/scala/iliad/shapes.scala
@@ -18,6 +18,8 @@ case class Rect[A](x0y0: VectorD[nat._2, A], dx: A, dy: A) {
   def bottomRight(implicit NA: Numeric[A]): VectorD[nat._2, A] = v"${x0y0.x + dx} ${x0y0.y}"
   def topRight(implicit NA: Numeric[A]): VectorD[nat._2, A] = v"${x0y0.x + dx} ${x0y0.y + dy}"
   def topLeft(implicit NA: Numeric[A]): VectorD[nat._2, A] = v"${x0y0.x} ${x0y0.y + dy}"
+  def width: A = dy
+  def height: A = dx
 
   def contains(xy: VectorD[nat._2, A])(implicit NA: Numeric[A]): Boolean = {
     val x0y0 = bottomLeft
