a9ec610af598605d19ebdfeb86c8d6ecaa37e881
Zainab Ali <zainab.ali.london@gmail.com>
2016-06-06 22:12:08 +0100

    Adding tests

--- kernel/src/main/scala/iliad/Enum.scala
+++ kernel/src/main/scala/iliad/Enum.scala
@@ -1,7 +1,9 @@
 package iliad
 
+import simulacrum.typeclass
+
 /** Represent discrete operations that can be performed on A */
-trait Enum[A] {
+@typeclass trait Enum[A] {
   def succ(x: A): A
   def pred(x: A): A
   def adj(x: A, y: A): Boolean = succ(x) == y
