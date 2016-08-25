cfe6eacc6884e320c8e64e054a8acaa1970fc735
zainab-ali <zainab.ali.london@gmail.com>
2016-08-25 14:04:45 +0100

    Adding monocle extras

--- core/src/main/scala/iliad/monocle.scala
+++ core/src/main/scala/iliad/monocle.scala
@@ -7,6 +7,10 @@ import cats._
 import cats.functor.Invariant
 import cats.data._
 
+import scala.reflect._
+
+import CatsExtra._
+
 object MonocleExtra {
 
   implicit def toStateTOps[F[_]: Monad, S, A](
@@ -28,7 +32,13 @@ object MonocleExtra {
   implicit def lensInvariant[S]: Invariant[Lens[S, ?]] = new LensInvariant[S]
   implicit def lensCartesian[S]: Cartesian[Lens[S, ?]] = new LensCartesian[S]
 
-  implicit def getterApply[S]: Apply[Getter[S, ?]] = new GetterApply[S]
+  implicit def getterMonad[S]: Monad[Getter[S, ?]] = new GetterMonad[S]
+
+  implicit def getterOps[S, A](getter: Getter[S, A]): GetterOps[S, A] = new GetterOps(getter)
+  implicit def setterOps[S, A](setter: Setter[S, A]): SetterOps[S, A] = new SetterOps(setter)
+
+  implicit def lensGetterOps[S, A](lens: Lens[S, A]): GetterOps[S, A] = new GetterOps(lens.asGetter)
+  implicit def lensSetterOps[S, A](lens: Lens[S, A]): SetterOps[S, A] = new SetterOps(lens.asSetter)
 }
 
 final class LensInvariant[S] extends Invariant[Lens[S, ?]] {
@@ -45,9 +55,37 @@ final class LensCartesian[S] extends Cartesian[Lens[S, ?]] {
     }
 }
 
-final class GetterApply[S] extends Apply[Getter[S, ?]] {
- def map[A, B](fa: Getter[S, A])(f: A => B): Getter[S, B] =
+final class GetterMonad[S] extends Monad[Getter[S, ?]] {
+  def pure[A](x: A): monocle.Getter[S,A] = Getter[S, A](_ => x)
+
+  def flatMap[A, B](fa: Getter[S,A])(f: A => Getter[S,B]): monocle.Getter[S,B] = 
+    Getter[S, B](s => f(fa get s) get s)
+
+  override def map[A, B](fa: Getter[S, A])(f: A => B): Getter[S, B] =
    Getter[S, B]((fa get _) andThen f)
-  def ap[A, B](ff: Getter[S, A => B])(fa: Getter[S, A]): Getter[S, B] =
+
+  override def ap[A, B](ff: Getter[S, A => B])(fa: Getter[S, A]): Getter[S, B] =
     Getter[S, B](s => (ff get s)(fa get s))
+}
+
+final class GetterOps[S, A](val getter: Getter[S, A]) extends AnyVal {
+  def view[F[_]](implicit M: MonadReader[F, S]): F[A] =
+    M.reader(getter get)
+
+  def inspect[F[_]](implicit M: MonadState[F, S]): F[A] =
+    M.inspect(getter get)
+}
+
+final class SetterOps[S, A](val setter: Setter[S, A]) extends AnyVal {
+  def assign[F[_]](a: A)(implicit M: MonadState[F, S]): F[Unit] =
+    M.modify(setter set a)
+  def modifying[F[_]](f: A => A)(implicit M: MonadState[F, S]): F[Unit] =
+    M.modify(setter modify f)
+}
+
+object OptionalExtra {
+  def narrow[A, B <: A](implicit ct: ClassTag[B]): Optional[A, B] = Optional[A, B]({
+    case b: B => Some(b)
+    case _ => None
+  })(b => _ => b)
 }
--- core/src/test/scala/iliad/MonocleTests.scala
+++ core/src/test/scala/iliad/MonocleTests.scala
@@ -9,6 +9,7 @@ import org.scalatest._
 import org.scalatest.prop._
 
 import cats._
+import cats.data._
 import cats.laws.discipline._
 import cats.laws.discipline.arbitrary._
 import cats.kernel.laws._
@@ -17,11 +18,12 @@ import cats.implicits._
 import org.scalacheck._
 
 import MonocleExtra._
+import CatsExtra._
 
 case class Person(name: Name)
 case class Name(first: String, second: String)
 
-trait MonocleTests {
+object Person {
   val _name: Lens[Person, Name] = GenLens[Person](_.name)
   val _firstName: Lens[Person, String] = _name ^|-> GenLens[Name](_.first)
   val _secondName: Lens[Person, String] = _name ^|-> GenLens[Name](_.second)
@@ -33,10 +35,9 @@ trait MonocleTests {
       second <- s
     } yield Person(Name(first, second))
   }
-
 }
 
-class LensTests extends FunSuite with Discipline with MonocleTests {
+class LensTests extends FunSuite with Discipline {
 
   import cats.laws.discipline.eq._
 
@@ -46,15 +47,16 @@ class LensTests extends FunSuite with Discipline with MonocleTests {
   implicit val iso = CartesianTests.Isomorphisms.invariant[Lens[Person, ?]]
 
   implicit val lensArbitrary: Arbitrary[Lens[Person, String]] = Arbitrary {
-    val boolean = implicitly[Arbitrary[Boolean]].arbitrary
-    boolean map (b => if(b) _firstName else _secondName)
+    val boolean = Arbitrary.arbitrary[Boolean]
+    boolean map (b => if(b) Person._firstName else Person._secondName)
   }
 
   checkAll("Lens[Person, String]", CartesianTests[Lens[Person, ?]].cartesian[String, String, String])
   checkAll("Cartesian[Lens[Person, ?]]", SerializableTests.serializable(Cartesian[Lens[Person, ?]]))
 }
 
-class GetterTests extends FunSuite with Discipline with MonocleTests {
+class GetterTests extends FunSuite with Discipline
+    with GeneratorDrivenPropertyChecks with Matchers {
 
   import cats.laws.discipline.eq._
 
@@ -62,8 +64,8 @@ class GetterTests extends FunSuite with Discipline with MonocleTests {
     Eq.by[Getter[S, A], S => A](_.get)
 
   implicit val getterArbitrary: Arbitrary[Getter[Person, String]] = Arbitrary {
-    val boolean = implicitly[Arbitrary[Boolean]].arbitrary
-    boolean map (b => if(b) _firstName.asGetter else _secondName.asGetter)
+    val boolean = Arbitrary.arbitrary[Boolean]
+    boolean map (b => if(b) Person._firstName.asGetter else Person._secondName.asGetter)
   }
 
   implicit val getterFArbitrary: Arbitrary[Getter[Person, String => String]] = Arbitrary {
@@ -71,6 +73,95 @@ class GetterTests extends FunSuite with Discipline with MonocleTests {
     fa.map(f => Getter[Person, String => String](_ => f))
   }
 
-  checkAll("Getter[Person, String]", ApplyTests[Getter[Person, ?]].apply[String, String, String])
-  checkAll("Apply[Getter[Person, ?]]", SerializableTests.serializable(Apply[Getter[Person, ?]]))
+  checkAll("Getter[Person, String]", MonadTests[Getter[Person, ?]].monad[String, String, String])
+  checkAll("Monad[Getter[Person, ?]]", SerializableTests.serializable(Monad[Getter[Person, ?]]))
+
+  test("GetterOps#view creates a MonadReader") {
+    //A MonadReader exists
+    MonadReader[Reader[Person, ?], Person]
+
+    val getter = Person._firstName.asGetter
+    val view = getter.view[Reader[Person, ?]]
+    val reader = MonadReader[Reader[Person, ?], Person].reader(getter.get _)
+
+    forAll { (p: Person) =>
+      view.run(p) should  === (reader.run(p))
+    }
+  }
+
+  test("GetterOps#inspect creates a MonadState") {
+    //A MonadState exists
+    MonadState[State[Person, ?], Person]
+
+    val getter = Person._firstName.asGetter
+    val inspect = getter.inspect[State[Person, ?]]
+    val state = MonadState[State[Person, ?], Person].inspect(getter.get _)
+
+    forAll { (p: Person) =>
+      inspect.run(p).value should  === (state.run(p).value)
+    }
+  }
+}
+
+
+class SetterTests extends FunSuite with GeneratorDrivenPropertyChecks with Matchers {
+  
+  //A MonadState exists
+  val S = MonadState[State[Person, ?], Person]
+    
+  test("SetterOps#assign creates a MonadState#modify") {
+    val setter = Person._firstName.asSetter
+
+    forAll { (name: String, p: Person) =>
+      val assign = setter.assign[State[Person, ?]](name)
+      val state = S.modify(setter.set(name))
+      assign.run(p).value should === (state.run(p).value)
+    }
+  }
+  
+  test("SetterOps#modifying creates a MonadState#modify") {
+    val setter = Person._firstName.asSetter
+    forAll { (f: String => String, p: Person) =>
+      val modifying = setter.modifying[State[Person, ?]](f)
+      val state = S.modify(setter.modify(f))
+      modifying.run(p).value should === (state.run(p).value)
+    }
+  }
+}
+
+class OptionalTests extends FunSuite with GeneratorDrivenPropertyChecks with Matchers {
+
+  sealed trait Animal
+  case class Cat(name: String) extends Animal
+  case class Dog(name: String) extends Animal
+
+  implicit val catArbitrary: Arbitrary[Cat] = Arbitrary { Arbitrary.arbitrary[String].map(Cat(_)) }
+  val dogArbitrary: Arbitrary[Dog] = Arbitrary { Arbitrary.arbitrary[String].map(Dog(_)) }
+
+  implicit val animalArbitrary: Arbitrary[Animal] = Arbitrary {
+    Arbitrary.arbitrary[Boolean].flatMap { b => 
+      if(b) catArbitrary.arbitrary else dogArbitrary.arbitrary
+    }
+  }
+
+  test("OptionalExtra#narrow#getOption should exist if the subtype exists") {
+    val narrow = OptionalExtra.narrow[Animal, Cat]
+    
+    forAll { (animal : Animal) =>
+      animal match {
+        case _ : Dog => 
+          narrow.getOption(animal) should === (None)
+        case c : Cat => 
+          narrow.getOption(animal) should === (Some(c))
+      }
+    }
+  }
+
+  test("OptionalExtra#narrow#set should always set a value") {
+    val narrow = OptionalExtra.narrow[Animal, Cat]
+    
+    forAll { (cat : Cat, animal: Animal) =>
+      narrow.set(cat)(animal) should === (cat)
+    }
+  }
 }
